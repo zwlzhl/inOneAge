5d41918edd141b4891799df5f33b0e015d5b95b4
zhangwenlong <2543068611@qq.com>
2019-08-08 21:11:04 +0800

    合并

--- sign/src/main.js
+++ sign/src/main.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-<<<<<<< HEAD
-import store from './store';
-Vue.config.productionTip = false
-App.mpType = 'app'
-
-Vue.prototype.$store=store;
-=======
 import store from './store'
 
 Vue.config.productionTip = false
@@ -14,6 +7,5 @@ App.mpType = 'app'
 
 Vue.prototype.$store = store;
 
->>>>>>> zwl
 const app = new Vue(App)
 app.$mount()
