955932afff9facc569d7c9c045ca2c19753caafc
zhangwenlong <2543068611@qq.com>
2019-08-08 14:10:05 +0800

    设置请求头

--- sign/src/utils/request.js
+++ sign/src/utils/request.js
@@ -1,21 +1,27 @@
-var Fly = require("flyio/dist/npm/wx")
-const fly = new Fly;
+import Fly from "flyio/dist/npm/wx"
 
-//定义公共headers
-// fly.config.headers={
-//   "content-type":"application/x-www-form-urlencoded"
-// }
+export let fly = new Fly
+
 //设置超时
 fly.config.timeout = 10000;
 //设置请求基地址
-fly.config.baseURL = "https://sign.jasonandjay.com"
+fly.config.baseURL = 'https://sign.jasonandjay.com/'
+// fly.config.baseURL = "http://123.206.55.50:7001/"
+// fly.config.baseURL = "http://169.254.12.68:7001/"
+// fly.config.baseURL = 'http://127.0.0.1:7001/'
 
+const HOST = 'https://127.0.0.1' // 更改
 //添加请求拦截器
 fly.interceptors.request.use((request) => {
+  // 把openid放在请求头部
+  let openid = wx.getStorageSync('openid');
+  if (openid) {
+    request.headers['openid'] = openid;
+  }
   //给所有请求添加自定义header
-  request.headers["X-Tag"] = "flyio";
+  // request.headers["Cookie"] = map(cookies, (v, k) => k + '=' + v).join(';')
   //打印出请求体
-  console.log(request.body)
+  // console.log(request.body)
   //终止请求
   //var err=new Error("xxx")
   //err.request=request
@@ -28,6 +34,15 @@ fly.interceptors.request.use((request) => {
 //添加响应拦截器，响应拦截器会在then/catch处理之前执行
 fly.interceptors.response.use(
   (response) => {
+    if (response.request.url.indexOf(HOST) == 0) {
+      let hcks = response.headers['set-cookie'] || response.headers['Set-Cookie']
+      if (hcks != null) {
+        hcks.forEach(v => {
+          let ck = v.split(';')[0].split('=')
+          cookies[ck[0]] = ck[1]
+        })
+      }
+    }
     //只将请求结果的data字段返回
     return response.data
   },
@@ -37,4 +52,4 @@ fly.interceptors.response.use(
   }
 )
 
-export default fly;
+export default fly
