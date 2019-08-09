cc23bad2ee123f501da919841c7fcb3c78731288
mopan <1547025691@qq.com>
2019-08-09 17:33:42 +0800

    模糊搜索

--- sign/src/pages/Message/index.vue
+++ sign/src/pages/Message/index.vue
@@ -4,47 +4,67 @@
             <span>北京</span>
             <input type="text" placeholder="请输入搜索地址" v-model="address" focus>
         </header>
+        <ul>
+            <li v-for="(item,index) in addressList" :key="index" @click="select(index)" hover-class="hover">
+                <image src="/static/images/logo.png"></image>
+                <div>
+                    <p>{{item.title}}</p>
+                    <p>{{item.address}}</p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
-import {debounce} from '@/utils/bounch'
-import {mapActions,mapMutations} from 'vuex'
+import {debounce} from '@/utils/bounch';
+import '@/pages/index.vue';
+import {mapState,mapActions,mapMutations} from 'vuex'
 export default {
     data(){
         return{
              address: '',
-             suggestion:[]
         }
     },
-
-    watch:{
-        address(val,oldval){  
-            this.search(val)
-            console.log(val)
-        }
+    computed:{
+        ...mapState({
+           addressList:state=>state.message.searchList
+        })
     },
     methods:{
         ...mapMutations({
-         updateState:'message/updateState'
+         updateState:'message/addressList'
         }),
-     
+        ...mapActions({
+             submitMessage: 'message/getSuggestion'
+        }),
+         select(index){
+             let Address=this.addressList[index].address
+             console.log(Address)
+             this.updateState({
+                addressList:this.addressList[index]
+             })
+               wx.navigateTo({
+                   url:'../AddExam/main?address='+Address
+                })
+         }
+    },
+    mounted(){
+      this.submitMessage('八维')
     },
 
-//     created () {
-//     var that = this;
-//     // 使用函数防抖控制事件触发频率
-//     this.search = debounce((val)=>{
-//         // console.log('ff')
-//       this.search({
-//         keyword: val,
-//         region: '北京',
-//         success: function (res) {
-//           console.log(res);
-//           that.suggestion = res.data;
-//         }
-//       })
-//     }, 300);
-//   }
+    created () {
+    var that = this;
+    // 使用函数防抖控制事件触发频率
+    this.search = debounce((val)=>{
+      this.search({
+        keyword: val,
+        region: '北京',
+        success: function (res) {
+          that.suggestion = res.data;
+        }
+      })
+    }, 300);
+  }
     
 }
 </script>
@@ -52,6 +72,8 @@ export default {
     .wrap{
         width:100%;
         height:100%;
+        display:flex;
+        flex-direction:column;
         header{
             width:100%;
             height:120rpx;
@@ -70,6 +92,32 @@ export default {
                height:120rpx;
                line-height:120rpx;
            }
+        }
+        ul{
+            width:100%;
+            height:100%;
+            flex:1;
+            font-size:35rpx;
+            overflow:auto;
+            li{
+                height:150rpx;
+                border-bottom:1rpx solid #ccc;
+                display:flex;
+                image{
+                    // flex:2;
+                    padding:20rpx 30rpx;
+                    width:40rpx;
+                    height:80rpx;
+
+                }
+                div{
+                    flex:7;
+                    p{
+                        padding-top:15rpx;
+                    }
+                }
+            }
+
         }
     }
 </style>
